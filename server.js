6ed36bf47cc089fa861c7499dc5f9264baba595b
profmouse <tappiera00@gmail.com>
2016-09-28 21:34:29 +0200

    testing heroku prod build

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ module.exports = {
     const app = express()
     const indexPath = path.join(__dirname, 'index.html')
     const publicPath = express.static(path.join(__dirname, 'public'))
+    const srcPath = express.static(path.join(__dirname, 'src'))
 
     app.use('/public', publicPath)
+    app.use('/src', srcPath)
     app.get('/', function (_, res) { res.sendFile(indexPath) })
 
     return app
